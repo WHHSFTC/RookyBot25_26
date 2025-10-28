364fc6a6c9846854e0d0de8fd4b7d836da4651ea
Bahand Abdulrahman <dudesedrit@gmail.com>
2025-10-28 08:50:10 -0400

    Created Drivetrain Class and implemented Driver 1 Gamepad

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/InitOpMode.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class InitOpMode extends OpMode {
+    protected Gamepad gamepad1 = new Gamepad();
+
+    // This method runs once when the "INIT" button is pressed on Driver Station
+    @Override
+    public void init() {
+    }
+
+    public void loop() {
+
+    }
+}
