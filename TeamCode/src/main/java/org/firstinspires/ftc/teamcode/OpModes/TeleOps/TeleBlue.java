7e4be60274156d6e07cc0b5bb5c8aa4df29351a4
Bahand Abdulrahman <dudesedrit@gmail.com>
2025-11-09 11:27:59 -0500

    Created Transfer Class. Drivetrain Code is operational and ready for use.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/TeleBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/TeleBlue.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode.OpModes.TeleOps;
 
-public class TeleBlue {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.OpModes.InitOpMode;
+import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
+@TeleOp(name = "TeleBlue", group = "OpModes")
+public class TeleBlue extends InitOpMode {
+    @Override
+    public void loop() {
+        drivetrain.moveWithGamepad();
+    }
 }
