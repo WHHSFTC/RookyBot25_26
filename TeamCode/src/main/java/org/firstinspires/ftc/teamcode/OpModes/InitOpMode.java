e420601aef6f91e5b44258270269aa9b578eb7f7
Bahand Abdulrahman <dudesedrit@gmail.com>
2025-11-13 20:54:16 -0500

    Beginning of Official Rooky Code. Cleared all remnants of competition bot to allow for rookies to make their own code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/InitOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/InitOpMode.java
@@ -6,13 +6,11 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
 
 public class InitOpMode extends OpMode {
-    protected Drivetrain drivetrain;
-    protected Gamepad gamepad1 = new Gamepad();
 
     // This method runs once when the "INIT" button is pressed on Driver Station
     @Override
     public void init() {
-        drivetrain = new Drivetrain(hardwareMap, gamepad1);
+
 
     }
 
