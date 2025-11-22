e9c5a217504102f63ac5c1d9e99b3957d4b299a6
Yab1s <yabsirayh1186@gmail.com>
2025-11-22 11:01:23 -0800

    Bahand Helped rookies make servo control exist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/TeleBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/TeleBlue.java
@@ -2,13 +2,21 @@ package org.firstinspires.ftc.teamcode.OpModes.TeleOps;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.OpModes.InitOpMode;
 import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
-
+import com.qualcomm.robotcore.hardware.Gamepad;
 @TeleOp(name = "TeleBlue", group = "OpModes")
 public class TeleBlue extends InitOpMode {
+    Servo sideFlipperLeft;
+    Double openFlipperPosition = 0.33;
+    Double closedFlipperPosition = 0.0;
     @Override
     public void loop() {
+        sideFlipperLeft = hardwareMap.get(Servo.class, "sideFlipperLeft");
+        sideFlipperLeft.setPosition(Math.abs(gamepad1.left_stick_x));
+        telemetry.addData("Servo Postiion:", sideFlipperLeft.getPosition());
+
     }
 }
