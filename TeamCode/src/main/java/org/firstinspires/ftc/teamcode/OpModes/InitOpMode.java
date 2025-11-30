a6a70e97be02d87712499b9083321ce0f69a795b
Yab1s <yabsirayh1186@gmail.com>
2025-11-29 17:26:22 -0800

    Updated Rookie Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/InitOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/InitOpMode.java
@@ -1,17 +1,42 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
 
 public class InitOpMode extends OpMode {
+    public Servo sideFlipperLeft, sideFlipperRight, middleFlipper;
+    public DcMotor Flywheel,Intake;
+    public Double openFlipperPosition = 0.4;
+    public Double closedFlipperPosition = 0.0;
+    public Double middleFlipperPosition = 0.22;
+    public Double FlywheelSpeed = 0.0;
+    public DcMotor leftFrontMotor, rightFrontMotor, leftBackMotor, rightBackMotor;
 
-    // This method runs once when the "INIT" button is pressed on Driver Station
     @Override
     public void init() {
-
-
+        sideFlipperLeft = hardwareMap.get(Servo.class, "sideFlipperLeft");
+        sideFlipperRight = hardwareMap.get(Servo.class, "sideFlipperRight");
+        middleFlipper = hardwareMap.get(Servo.class, "middleFlipper");
+        Flywheel = hardwareMap.get(DcMotor.class, "Flywheel");
+        Intake = hardwareMap.get(DcMotor.class, "Intake");
+        //DRive train motors
+        leftFrontMotor = hardwareMap.get(DcMotor.class, "leftFrontMotor");
+        rightFrontMotor = hardwareMap.get(DcMotor.class, "rightFrontMotor");
+        leftBackMotor = hardwareMap.get(DcMotor.class, "leftBackMotor");
+        rightBackMotor = hardwareMap.get(DcMotor.class, "rightBackMotor");
+        //Flippers
+        sideFlipperLeft.setPosition(openFlipperPosition);
+        sideFlipperRight.setPosition(closedFlipperPosition);
+        middleFlipper.setPosition(closedFlipperPosition);
+        //Drivetrain forward and backwards
+        leftFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightBackMotor.setDirection(DcMotor.Direction.FORWARD);
     }
 
     public void loop() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/TeleBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOps/TeleBlue.java
@@ -2,21 +2,85 @@ package org.firstinspires.ftc.teamcode.OpModes.TeleOps;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.OpModes.InitOpMode;
 import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
 import com.qualcomm.robotcore.hardware.Gamepad;
+
 @TeleOp(name = "TeleBlue", group = "OpModes")
 public class TeleBlue extends InitOpMode {
-    Servo sideFlipperLeft;
-    Double openFlipperPosition = 0.33;
-    Double closedFlipperPosition = 0.0;
+
+boolean intakeState = false;
     @Override
     public void loop() {
-        sideFlipperLeft = hardwareMap.get(Servo.class, "sideFlipperLeft");
-        sideFlipperLeft.setPosition(Math.abs(gamepad1.left_stick_x));
-        telemetry.addData("Servo Postiion:", sideFlipperLeft.getPosition());
 
+
+        if (gamepad1.right_bumper) {
+            sideFlipperRight.setPosition(openFlipperPosition);
+        } else {
+            sideFlipperRight.setPosition(closedFlipperPosition);
+        }
+        if (gamepad1.left_bumper) {
+            sideFlipperLeft.setPosition(closedFlipperPosition);
+        } else {
+            sideFlipperLeft.setPosition(openFlipperPosition);
+        }
+        if (gamepad1.dpad_right) {
+            middleFlipper.setPosition(middleFlipperPosition);
+        }
+        if (gamepad1.dpad_left) {
+            middleFlipper.setPosition(-middleFlipperPosition);
+        }
+        if (gamepad1.dpad_up) {
+          FlywheelSpeed= FlywheelSpeed + 1000.0;
+          Flywheel.setPower(FlywheelSpeed);
+        } else if (gamepad1.dpad_down) {
+           Flywheel.setPower(0);
+        }
+        if (gamepad1.dpad_up) {
+            if (FlywheelSpeed < 6000.0) {
+                FlywheelSpeed = FlywheelSpeed + 1000.0;
+            } else if (FlywheelSpeed >= 6000.0) {
+                FlywheelSpeed = 6000.0;
+            }
+            Flywheel.setPower(FlywheelSpeed);
+        }
+        if (gamepad1.dpad_down) {
+            Flywheel.setPower(0);
+        }
+        if (gamepad1.a && intakeState == false) {
+            //Code for when intake is turning on
+            intakeState = true;
+            Intake.setPower(1150);
+        } else if (gamepad1.a && intakeState == true) {
+            //Code for when intake is turning off
+            intakeState = false;
+            Intake.setPower(0);
+        }
+
+        mecanumDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
     }
+
+    public void mecanumDrive(float v, float leftStickX, float rightStickX) {
+        double y = -gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        double frontLeftPower = (y + x + rx) / denominator;
+        double backLeftPower = (y - x + rx) / denominator;
+        double frontRightPower = (y - x - rx) / denominator;
+        double backRightPower = (y + x - rx) / denominator;
+
+        leftFrontMotor.setPower(frontLeftPower);
+        rightFrontMotor.setPower(frontRightPower);
+        leftBackMotor.setPower(backLeftPower);
+        rightBackMotor.setPower(backRightPower);
+    }
+
 }
+
